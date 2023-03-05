964637b7fd1f0ea69c709ae657dc9b38742e10f3
Rafael <rafaelcbs1@hotmail.com>
2023-03-05 17:34:21 -0300

    quase

--- comparacoes.js
+++ comparacoes.js
@@ -16,3 +16,8 @@ console.log(typeof texto)
 
 Number()
 String()
+
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
